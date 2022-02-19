00611b35f83229eea40b4a8415a7108a3696b19f
Aishu203100 <79292874+Aishu203100@users.noreply.github.com>
2022-02-18 20:44:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 music = " ";
 music2 = " ";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function preload()
 {
     music = loadSound("music.mp3")
@@ -12,8 +16,33 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded()
+{
+
+}
+function gotPoses(results)
+{
+    if(results[0].length > 0)
+    {
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("LeftWristX = " + leftWristX + " LeftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("RightWristX = " + rightWristX + " RightWristY = " + rightWristY);
+    }
 }
 function draw()
 {
-    Image(video,0,0,600,500);
+    image(video,0,0,600,500);
+}
+function play()
+{
+    music.play();
+    music.setVolume(1);
+    music.rate(1);
 }
